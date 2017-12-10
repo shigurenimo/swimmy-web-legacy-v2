b8a4d2c90e465fac0e17ead2865d709a5adf2a55
uufish <uufish.io@gmail.com>
2017-12-11 01:59:55 +0900

    update: タイムラインのUIの追加

--- src/app/view-home/view-home.component.ts
+++ src/app/view-home/view-home.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import {
+  AngularFirestore,
+  AngularFirestoreCollection
+} from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+
+import { Post } from '../models/Post';
 
 @Component({
   selector: 'app-view-home',
   templateUrl: './view-home.component.html',
   styleUrls: ['./view-home.component.css']
 })
-export class ViewHomeComponent implements OnInit {
+export class ViewHomeComponent {
+  col$: AngularFirestoreCollection<Post>;
+  docs$: Observable<Post[]>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private afs: AngularFirestore) {
+    this.getCol();
+    this.getDocs();
   }
 
+  getCol() {
+    this.col$ = this.afs.collection<Post>('posts');
+  }
+
+  getDocs() {
+    this.docs$ = this.col$.valueChanges();
+  }
 }
