5f3e11dc93ee8a8ce55ccea315e98f6591159c8e
uufish <uufish.io@gmail.com>
2017-12-11 01:59:27 +0900

    update: AngularFire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,16 @@
+// Angular core modules
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 
+// Modules not from angular core
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
 import { environment } from '../environments/environment';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
@@ -12,6 +19,8 @@ import { SidenavComponent } from './sidenav/sidenav.component';
 import { ViewLoginComponent } from './view-login/view-login.component';
 import { ViewConfigComponent } from './view-config/view-config.component';
 import { ViewInfoComponent } from './view-info/view-info.component';
+import { ViewUserComponent } from './view-user/view-user.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
 @NgModule({
   declarations: [
@@ -20,9 +29,14 @@ import { ViewInfoComponent } from './view-info/view-info.component';
     SidenavComponent,
     ViewLoginComponent,
     ViewConfigComponent,
-    ViewInfoComponent
+    ViewInfoComponent,
+    ViewUserComponent,
+    TimelineComponent
   ],
   imports: [
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
     AppRoutingModule,
     BrowserModule,
     NoopAnimationsModule,
