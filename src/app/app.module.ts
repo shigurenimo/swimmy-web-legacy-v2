7e61c991f0dac780ccfcca55661fd97b5453fce3
uufish <uufish.io@gmail.com>
2017-12-11 01:01:51 +0900

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
 import { ViewHomeComponent } from './view-home/view-home.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
+import { ViewLoginComponent } from './view-login/view-login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ViewHomeComponent,
-    SidenavComponent
+    SidenavComponent,
+    ViewLoginComponent
   ],
   imports: [
     AppRoutingModule,
