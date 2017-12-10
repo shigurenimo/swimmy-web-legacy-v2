3f4518e1238c04c231f403db6d464da412880842
uufish <uufish.io@gmail.com>
2017-12-11 01:59:37 +0900

    update: ルーティングの追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ViewConfigComponent } from './view-config/view-config.component';
 import { ViewHomeComponent } from './view-home/view-home.component';
+import { ViewInfoComponent } from './view-info/view-info.component';
+import { ViewUserComponent } from './view-user/view-user.component';
 
 const routes: Routes = [
+  {path: 'config', component: ViewConfigComponent},
+  {path: 'info', component: ViewInfoComponent},
+  {path: 'user/:id', component: ViewUserComponent},
   {path: '', component: ViewHomeComponent}
 ];
 
