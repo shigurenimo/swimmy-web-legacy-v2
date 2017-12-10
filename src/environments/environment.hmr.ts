e815462245a94e4b896ec08291ea8b5ac536d9b0
uufish <uufish.io@gmail.com>
2017-12-10 23:58:22 +0900

    update: firebase config

--- src/environments/environment.hmr.ts
+++ src/environments/environment.hmr.ts
@@ -1,6 +1,13 @@
 export const environment = {
   production: false,
   hmr: true,
-  firebase: {},
-  function: ''
+  firebase: {
+    apiKey: 'AIzaSyBmZ8J8ThdpIlxGirqm9bTZocT-s9pqVMs',
+    authDomain: 'swimmy-171720.firebaseapp.com',
+    databaseURL: 'https://swimmy-171720.firebaseio.com',
+    projectId: 'swimmy-171720',
+    storageBucket: 'swimmy-171720.appspot.com',
+    messagingSenderId: '105044844269'
+  },
+  function: 'http://localhost:5000/swimmy-171720/us-central1'
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,13 @@
 export const environment = {
   production: true,
   hmr: false,
-  firebase: {},
-  function: ''
+  firebase: {
+    apiKey: 'AIzaSyBmZ8J8ThdpIlxGirqm9bTZocT-s9pqVMs',
+    authDomain: 'swimmy-171720.firebaseapp.com',
+    databaseURL: 'https://swimmy-171720.firebaseio.com',
+    projectId: 'swimmy-171720',
+    storageBucket: 'swimmy-171720.appspot.com',
+    messagingSenderId: '105044844269'
+  },
+  function: 'https://us-central1-swimmy-171720.cloudfunctions.net'
 };
