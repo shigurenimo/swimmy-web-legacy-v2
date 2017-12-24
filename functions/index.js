bbc6ac95f200bb5e5ebf79d8cfa004d3fd13d2e5
uufish <uufish.io@gmail.com>
2017-12-25 03:10:56 +0700

    update: https/addPost

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,10 @@
+const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+const FUNCTION_NAME = process.env.FUNCTION_NAME;
+
+if (!FUNCTION_NAME || FUNCTION_NAME === 'addPost') {
+  exports.addPost = require('./https/addPost');
+}
