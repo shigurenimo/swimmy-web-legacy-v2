b832b37231001cc55b59ab86eef9c997c36754d1
uufish <uufish.io@gmail.com>
2017-12-25 03:11:38 +0700

    update: 投稿機能の追加

--- src/app/editor-post/editor-post.component.ts
+++ src/app/editor-post/editor-post.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FunctionsService } from '../services/functions.service';
+import {
+  FormBuilder, FormControl, FormGroup,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-editor-post',
@@ -6,10 +11,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editor-post.component.css']
 })
 export class EditorPostComponent implements OnInit {
+  public options: FormGroup;
 
-  constructor() { }
+  public content = new FormControl('', [Validators.maxLength(400)]);
+
+  private inProgress = false;
+
+  get contentError() {
+    if (this.content.hasError('failure')) {
+      return '失敗しました';
+    }
+    return '';
+  }
+
+  constructor(
+    private fns: FunctionsService,
+    private fb: FormBuilder) {
+    this.options = fb.group({
+      hideRequired: false,
+      floatLabel: 'auto'
+    });
+  }
 
   ngOnInit() {
   }
 
+  onCreatePostClick() {
+    const payload = {
+      content: this.content.value
+    };
+    this.fns.createPost(payload)
+      .then(() => {
+        this.content.setValue('');
+      })
+      .catch(err => {
+        console.error(err);
+        this.content.setErrors({failure: true});
+      });
+  }
 }
--- src/app/services/functions.service.ts
+++ src/app/services/functions.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@angular/core';
 
+import { AngularFireAuth } from 'angularfire2/auth';
+import axios from 'axios';
+
+import { environment } from '../../environments/environment';
+
 @Injectable()
 export class FunctionsService {
+  constructor(private afa: AngularFireAuth) {
+  }
 
-  constructor() { }
+  public createPost(payload) {
+    return this.getIdToken()
+      .then(idToken => {
+        axios({
+          method: 'POST',
+          url: `${environment.function}/addPost`,
+          headers: {
+            'authorization': `Bearer ${idToken}`,
+            'Content-Type': 'application/json'
+          },
+          data: payload
+        });
+      });
+  }
 
+  private getIdToken() {
+    return this.afa.auth.currentUser.getIdToken();
+  }
 }
