97297fb309096ee653ce8329d59ccb74a9aca268
uufish <uufish.io@gmail.com>
2017-12-25 22:09:22 +0700

    update: user/:id > :uid

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ViewConfigComponent } from './view-config/view-config.component';
 import { ViewHomeComponent } from './view-home/view-home.component';
 import { ViewInfoComponent } from './view-info/view-info.component';
-import { ViewUserComponent } from './view-user/view-user.component';
 import { ViewLoginComponent } from './view-login/view-login.component';
+import { ViewUserDetailComponent } from './view-user-detail/view-user-detail.component';
 
 const routes: Routes = [
   {path: 'config', component: ViewConfigComponent},
   {path: 'info', component: ViewInfoComponent},
-  {path: 'user/:id', component: ViewUserComponent},
   {path: 'login', component: ViewLoginComponent},
+  {path: ':uid', component: ViewUserDetailComponent},
   {path: '', component: ViewHomeComponent}
 ];
 
