9ad7f63cf361639413f42d627094af110561e1f2
uufish <uufish.io@gmail.com>
2018-01-08 00:02:13 +0700

    update: ユーザアイコンの追加

--- src/app/view-user-detail/view-user-detail.component.ts
+++ src/app/view-user-detail/view-user-detail.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from 'angularfire2/auth';
+
+import { UsersService } from '../services/users.service';
+import { User } from '../models/User';
 
 @Component({
   selector: 'app-view-user-detail',
   templateUrl: './view-user-detail.component.html',
   styleUrls: ['./view-user-detail.component.css']
 })
-export class ViewUserDetailComponent implements OnInit {
+export class ViewUserDetailComponent implements OnInit, OnDestroy {
+  public photoURL;
 
-  constructor() { }
+  private userSub;
 
-  ngOnInit() {
+  constructor(
+    private route: ActivatedRoute,
+    private users: UsersService,
+    public afAuth: AngularFireAuth) {
   }
 
+  public previewImage(e) {
+    const uid = this.afAuth.app.auth().currentUser.uid;
+    const file = e.target.files[0];
+    const ext = this.getExt(file.type);
+    if (!ext) {
+      return;
+    }
+    const fileName = `${uid}.${ext}`;
+    const filePath = `icons/${fileName}`;
+    const storageRef = firebase.storage().ref(filePath);
+    const task = storageRef.put(file);
+  }
+
+  public ngOnInit() {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      const uid = params.get('uid');
+      this.userSub = this.users.fetch(uid)
+        .subscribe((user: User) => {
+          this.photoURL = user.photoURL;
+        });
+    });
+  }
+
+  public ngOnDestroy() {
+    this.userSub.unsubscribe();
+  }
+
+  private getExt(type) {
+    switch (type) {
+      case 'image/png':
+        return 'png';
+      case 'image/jpeg':
+        return 'jpg';
+    }
+  }
 }
