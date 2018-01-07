74e5a3df634ecd17e1d7c7541dbe6df80595a829
uufish <uufish.io@gmail.com>
2018-01-07 23:49:12 +0700

    add

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@angular/core';
 
+import { AngularFirestore } from 'angularfire2/firestore';
+
+import { User } from '../models/User';
+
 @Injectable()
 export class UsersService {
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) { }
 
+  public fetch(id: string) {
+    return this.afs
+      .collection<User>('users')
+      .doc(id)
+      .snapshotChanges()
+      .map(snapshot => {
+        const payload = snapshot.payload;
+        if (!payload.exists) {
+          return;
+        }
+        return {
+          id: payload.id,
+          ...payload.data() as User
+        };
+      })
+      .share();
+  }
 }
