b2c59c1bd84e4e0c06563fb871833b0c352d33bc
uufish <uufish.io@gmail.com>
2018-01-07 23:59:47 +0700

    update: storage

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,14 @@ admin.initializeApp(functions.config().firebase);
 
 const FUNCTION_NAME = process.env.FUNCTION_NAME;
 
+// https
+
 if (!FUNCTION_NAME || FUNCTION_NAME === 'addPost') {
   exports.addPost = require('./https/addPost');
+}
+
+// storage
+
+if (!FUNCTION_NAME || FUNCTION_NAME === 'storage') {
+  exports.storage = require('./storage');
 }
