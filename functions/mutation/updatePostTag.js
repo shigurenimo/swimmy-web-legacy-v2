ba80e4c29422ce451cc2899c6393f988799e0075
uufish <uufish.io@gmail.com>
2018-01-29 08:42:52 +0700

    fix: like

--- functions/mutation/updatePostTag.js
+++ functions/mutation/updatePostTag.js
@@ -1,7 +1,7 @@
 const {updatePostTag} = require('../https/updatePostTags');
 
 exports.default = (root, args, context) => {
-  args.name = args.name || '+1';
+  args.name = args.name || 'like';
   if (!context.uid) return;
   return updatePostTag(args, context);
 };
