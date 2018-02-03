12bcd792d1bbd82af466bce6f0377e9e3673c86c
uufish <uufish.io@gmail.com>
2018-02-04 00:05:21 +0700

    update: apollo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HttpHeaders } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { HttpClientModule, HttpHeaders } from '@angular/common/http';
 
 import { AngularFireAuth, AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFireModule } from 'angularfire2';
@@ -12,42 +12,40 @@ import { Apollo, ApolloModule } from 'apollo-angular';
 import { HttpLink, HttpLinkModule } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { setContext } from 'apollo-link-context';
-
 import 'firebase/storage';
 
 import { environment } from '../environments/environment';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { EditorPostComponent } from './editor-post/editor-post.component';
+import { DialogLoginComponent } from './dialog-login/dialog-login.component';
+import { ListItemPostComponent } from './list-item-post/list-item-post.component';
 import { MaterialModule } from './material.module';
-import { ViewHomeComponent } from './view-home/view-home.component';
+import { FunctionsService } from './services/functions.service';
+import { UsersService } from './services/users.service';
+import { PostsService } from './services/posts.service';
 import { SidenavComponent } from './sidenav/sidenav.component';
+import { TimelineComponent } from './timeline/timeline.component';
+import { ViewHomeComponent } from './view-home/view-home.component';
 import { ViewLoginComponent } from './view-login/view-login.component';
 import { ViewConfigComponent } from './view-config/view-config.component';
 import { ViewInfoComponent } from './view-info/view-info.component';
 import { ViewUserComponent } from './view-user/view-user.component';
-import { TimelineComponent } from './timeline/timeline.component';
-import { ListItemPostComponent } from './list-item-post/list-item-post.component';
-import { EditorPostComponent } from './editor-post/editor-post.component';
-import { DialogLoginComponent } from './dialog-login/dialog-login.component';
-import { FunctionsService } from './services/functions.service';
 import { ViewUserDetailComponent } from './view-user-detail/view-user-detail.component';
-import { UsersService } from './services/users.service';
-import { PostsService } from './services/posts.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ViewHomeComponent,
-    SidenavComponent,
-    ViewLoginComponent,
-    ViewConfigComponent,
-    ViewInfoComponent,
-    ViewUserComponent,
-    TimelineComponent,
-    ListItemPostComponent,
     EditorPostComponent,
     DialogLoginComponent,
+    ListItemPostComponent,
+    SidenavComponent,
+    TimelineComponent,
+    ViewConfigComponent,
+    ViewHomeComponent,
+    ViewInfoComponent,
+    ViewLoginComponent,
+    ViewUserComponent,
     ViewUserDetailComponent
   ],
   imports: [
@@ -69,10 +67,12 @@ import { PostsService } from './services/posts.service';
   ],
   providers: [
     FunctionsService,
-    UsersService,
-    PostsService
+    PostsService,
+    UsersService
   ],
-  bootstrap: [AppComponent],
+  bootstrap: [
+    AppComponent
+  ],
   entryComponents: [
     DialogLoginComponent
   ],
@@ -85,29 +85,38 @@ export class AppModule {
     private apollo: Apollo,
     private httpLink: HttpLink,
     private afAuth: AngularFireAuth) {
-    const http = httpLink.create({
-      uri: environment.graphql,
-      method: 'GET'
-    });
-
-    const cache = new InMemoryCache();
-
-    const bearerHttp = setContext(() => {
-      if (afAuth.auth.currentUser) {
-        return afAuth.auth.currentUser.getIdToken()
+    const httpBearer = setContext(() => {
+      if (this.afAuth.auth.currentUser) {
+        return this.afAuth.auth.currentUser.getIdToken()
           .then((idToken) => {
             const bearer = `Bearer ${idToken}`;
-            return {
-              headers: new HttpHeaders().set('authorization', bearer)
-            };
+            const headers = new HttpHeaders().set('authorization', bearer);
+            return {headers};
           });
       } else {
         return {};
       }
     });
 
-    apollo.create({
-      link: bearerHttp.concat(http),
+    const cache = new InMemoryCache();
+
+    const httpQuery = httpLink.create({
+      uri: environment.graphql,
+      method: 'GET'
+    });
+
+    const httpMutation = httpLink.create({
+      uri: environment.graphql,
+      method: 'POST'
+    });
+
+    apollo.createDefault({
+      link: httpBearer.concat(httpQuery),
+      cache: cache
+    });
+
+    apollo.createNamed('mutation', {
+      link: httpBearer.concat(httpMutation),
       cache: cache
     });
   }
