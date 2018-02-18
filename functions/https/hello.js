fbfe322b3bd1b04df03897ae1c1623a4ce6c132d
uufish <uufish.io@gmail.com>
2018-02-19 01:26:14 +0700

    add

---
+++ functions/https/hello.js
@@ -0,0 +1,32 @@
+const cors = require('cors');
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+
+const failureResponse = require('../helpers/failureResponse').default;
+const getContext = require('../helpers/getContext').default;
+const successResponse = require('../helpers/successResponse').default;
+
+exports.default = functions.https.onRequest((request, response) => {
+  return cors()(request, response, () => {
+    return writeData().
+      then(() => {
+        return successResponse(response);
+      }).
+      catch((err) => {
+        return failureResponse(response, err);
+      });
+  });
+});
+
+const writeData = () => {
+  const firestore = admin.firestore();
+
+  const batch = firestore.batch();
+
+  for (let i = 0; i < 50; ++i) {
+    const nycRef = firestore.collection('hello').doc(i.toString());
+    batch.set(nycRef, {i: i});
+  }
+
+  return batch.commit();
+};
