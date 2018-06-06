a71e64084bb3b85f28f2960397c0b2d46687e60a
uufish <uufish.io@gmail.com>
2018-06-07 04:51:33 +0700

    add: #8

---
+++ src/app/services/browser.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot } from '@angular/router';
+
+import { Subject } from 'rxjs/Subject';
+
+@Injectable()
+export class BrowserService {
+  public appTitle: Subject<string> = new Subject();
+
+  constructor() {
+  }
+
+  public getAppTitle() {
+    return this.appTitle;
+  }
+
+  public updateSnapshot(snapshot: ActivatedRouteSnapshot) {
+    const {title} = snapshot.data;
+    this.appTitle.next(title);
+  }
+}
